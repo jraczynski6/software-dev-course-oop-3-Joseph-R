84e8307dcb5b612fe7f6090b31ae828f60a2b44f
Joseph R <raczynski.joseph616@gmail.com>
2026-01-13 19:19:12 -0600

    Create LibraryItem: fields and constructor

---
+++ src/main/java/org/example/LibraryItem.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class LibraryItem {
+
+    protected String title;
+    protected int year;
+    protected String author;
+
+    public LibraryItem(String title, int year, String author) {
+        this.title = title;
+        this.year = year;
+        this.author = author;
+    }
+}
